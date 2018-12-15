ab66d1d1bb7be1a09ed554dfd921eb02d04aa2a1
Daniel Santos <edandaniel@MacBook-Pro-de-Macbook-2.local>
2018-12-15 05:14:27 -0200

    calculo do total parcialmente feito, ajustado auto layout para o texto

--- ComprasUSA/SumViewController.swift
+++ ComprasUSA/SumViewController.swift
@@ -13,11 +13,21 @@ class SumViewController: UIViewController {
 
     @IBOutlet weak var lbRealSum: UILabel!
     @IBOutlet weak var lbDolarSum: UILabel!
+    
+    var totalBRL:Double = 0.0
+    var totalUSD:Double = 0.0
+    let ud = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sumProductValues()
+        updateScreenValues()
+    }
+    
+    func updateScreenValues(){
+        lbRealSum.text = String(totalBRL)
         
-
+        lbDolarSum.text = String(totalUSD)
     }
     
     var fetchedResultController: NSFetchedResultsController<Product>!
@@ -30,12 +40,29 @@ class SumViewController: UIViewController {
         let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptor]
-        
         fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         fetchedResultController.delegate = self
         
         do {
             try fetchedResultController.performFetch()
+            let products = try! context.fetch(fetchRequest)
+            let iof = ud.double(forKey: "iof")
+            let dolar = ud.double(forKey: "dolar")
+            totalBRL = 0
+            totalUSD = 0
+            for product in products{
+                totalBRL += product.value
+                //var tax = product.state.tax ?? 1
+                var value:Double = product.value
+                if(product.creditCard || iof != 0){
+                    totalUSD += (value * dolar) + (value * dolar)*(iof/100)
+                    //totalUSD += (value * dolar * tax)
+                }else{
+                    totalUSD += (value * dolar) + (value * dolar)*(iof/100)
+                    //totalUSD += (value * dolar * tax) + (value * dolar)(iof/100)
+                }
+            }
+            
         } catch {
             print(error.localizedDescription)
         }
@@ -44,6 +71,6 @@ class SumViewController: UIViewController {
 
 extension SumViewController: NSFetchedResultsControllerDelegate {
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        //tableView.reloadData()
+        
     }
 }
